5e187a7d385a18e2f7e2718cd3519ee07931ad77
Dawud2012 <dawud92012@hotmail.com>
2024-02-15 14:59:50 +0500

    test ci

---
+++ deploy.py
@@ -0,0 +1,6 @@
+# deploy.py
+def deploy():
+    print("Deploying to staging...")
+
+if __name__ == "__main__":
+    deploy()
---
+++ tests/test_code.py
@@ -0,0 +1,11 @@
+# tests/test_hello.py
+from ..code import main
+from io import StringIO
+import sys
+
+def test_hello(capsys):
+    captured_output = StringIO()
+    sys.stdout = captured_output
+    main()
+    sys.stdout = sys.__stdout__
+    assert captured_output.getvalue() == "Hello, World!\n"
